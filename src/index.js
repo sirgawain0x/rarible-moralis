8267fc55e2647d375dada57d8b9892ef3328988e
YosephKS <yosephkurniasoenggoro@gmail.com>
2021-10-08 22:10:25 +0700

    add eslint + prettier

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { MoralisProvider } from "react-moralis";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <MoralisProvider appId={process.env.REACT_APP_MORALIS_APP_ID} serverUrl={process.env.REACT_APP_MORALIS_SERVER_ID}>
-      <App />
-    </MoralisProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<StrictMode>
+		<MoralisProvider
+			appId={process.env.REACT_APP_MORALIS_APP_ID}
+			serverUrl={process.env.REACT_APP_MORALIS_SERVER_ID}
+		>
+			<App />
+		</MoralisProvider>
+	</StrictMode>,
+	document.getElementById("root"),
 );
 
 // If you want to start measuring performance in your app, pass a function
