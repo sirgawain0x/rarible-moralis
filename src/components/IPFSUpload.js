70957c3561855df63d394c88e3a6cc1650fc1ffb
YosephKS <yosephkurniasoenggoro@gmail.com>
2021-10-13 00:35:26 +0700

    adding full functionality for IPFS Uploading

--- src/components/IPFSUpload.js
+++ src/components/IPFSUpload.js
@@ -1,5 +1,6 @@
 import React, { forwardRef, useState } from "react";
 import { DropzoneArea } from "material-ui-dropzone";
+import PropTypes from "prop-types";
 import Grid from "@mui/material/Grid";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
@@ -28,14 +29,25 @@ const Transition = forwardRef(function Transition(props, ref) {
 	return <Slide direction="up" ref={ref} {...props} />;
 });
 
-const IPFSUpload = () => {
+const IPFSUpload = (props) => {
+	const { open, onCancel, onUpload } = props;
 	const V5Theme = useTheme();
 	const isSmallScreen = useMediaQuery(V5Theme.breakpoints.down("md"));
 	const [IPFSFiles, setIPFSFiles] = useState([]);
-	console.log(IPFSFiles);
+
+	const handleCancel = () => {
+		onCancel();
+		// Reset files to be empty
+		setIPFSFiles([]);
+	};
+
+	const handleUpload = () => {
+		onUpload(IPFSFiles);
+	};
+
 	return (
 		<Dialog
-			open
+			open={open}
 			fullWidth
 			fullScreen={isSmallScreen}
 			maxWidth="md"
@@ -45,6 +57,7 @@ const IPFSUpload = () => {
 			<DialogContent>
 				<MuiThemeProvider theme={theme}>
 					<DropzoneArea
+						fileLimit={1}
 						showPreviews
 						showPreviewsInDropzone={false}
 						onChange={(files) => setIPFSFiles(files)}
@@ -66,12 +79,22 @@ const IPFSUpload = () => {
 					}
 				>
 					<Grid item>
-						<Button variant="contained" color="secondary" fullWidth>
+						<Button
+							variant="contained"
+							color="secondary"
+							fullWidth
+							onClick={handleCancel}
+						>
 							Cancel
 						</Button>
 					</Grid>
 					<Grid item>
-						<Button variant="contained" color="primary" fullWidth>
+						<Button
+							variant="contained"
+							color="primary"
+							fullWidth
+							onClick={handleUpload}
+						>
 							Upload to IPFS
 						</Button>
 					</Grid>
@@ -79,6 +102,18 @@ const IPFSUpload = () => {
 			</DialogActions>
 		</Dialog>
 	);
+};
+
+IPFSUpload.propTypes = {
+	open: PropTypes.bool,
+	onCancel: PropTypes.func,
+	onUpload: PropTypes.func,
+};
+
+IPFSUpload.defaultProps = {
+	open: false,
+	onCancel: () => {},
+	onUpload: () => {},
 };
 
 export default IPFSUpload;
